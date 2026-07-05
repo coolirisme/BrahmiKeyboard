ec79b3c1bfff0c8aff29f90d07309777fca46e36
coolirisme <60419589+coolirisme@users.noreply.github.com>
2026-07-05 17:19:27 +0530

    Build SDK update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,59 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.ratsah.brahmi"
-    compileSdk {
-        version = release(37) {
-            minorApiLevel = 1
-        }
+  namespace = "com.ratsah.brahmi"
+  compileSdk {
+    version = release(37) {
+      minorApiLevel = 1
     }
+  }
 
-    defaultConfig {
-        applicationId = "com.ratsah.brahmi"
-        minSdk = 29
-        targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.3"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  defaultConfig {
+    applicationId = "com.ratsah.brahmi"
+    minSdk = 29
+    targetSdk = 37
+    versionCode = 5
+    versionName = "1.0.4"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-            optimization {
-                enable = true
-            }
-        }
+  buildTypes {
+    release {
+      signingConfig = signingConfigs.getByName("debug")
+      optimization {
+        enable = true
+      }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        compose = true
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  buildFeatures {
+    compose = true
+  }
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material3.adaptive)
-    implementation(libs.androidx.compose.material3.adaptive.layout)
-    implementation(libs.androidx.compose.material.icons.core)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.compose.material3)
+  implementation(libs.androidx.compose.material3.adaptive)
+  implementation(libs.androidx.compose.material3.adaptive.layout)
+  implementation(libs.androidx.compose.material.icons.core)
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.graphics)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  testImplementation(libs.junit)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(libs.androidx.junit)
+  debugImplementation(libs.androidx.compose.ui.test.manifest)
+  debugImplementation(libs.androidx.compose.ui.tooling)
 }
